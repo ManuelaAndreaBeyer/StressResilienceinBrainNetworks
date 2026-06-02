b4bf2e3917ebb7c67b75c34723917fc4fec32b46
Manuela Andrea Beyer <67337290+ManuelaAndreaBeyer@users.noreply.github.com>
2026-06-02 16:13:37 +0200

    Update Nengo_code_colection.py

--- Nengo_code_colection.py
+++ Nengo_code_colection.py
@@ -13,3 +13,42 @@ with model:
     nengo.Connection(a,b, synapse=0.2)
 
 #-----------------------------
+
+
+import nengo
+
+model = nengo.Network()
+
+
+# Day 2
+
+
+
+with model:
+    stim = nengo.Node([0])
+    a = nengo.Ensemble(n_neurons=50, dimensions=1)
+    nengo.Connection(stim,a)
+    
+    
+    #dx/dt=[1/tau_desired)*(u-x)
+    
+    tau_synapse=0.02
+    
+    tau_desired=0.5
+    
+    
+    b = nengo.Ensemble(n_neurons=100, dimensions=1)
+    
+    
+    def forward(u):
+        #input function
+        return (tau_synapse/tau_desired)*u
+    
+
+    nengo.Connection(a,b, synapse=tau_synapse, function=forward)
+    
+    def func(x):
+        #recurrent function
+        return -(tau_synapse/tau_desired)x*x
+        
+    nengo.Connection(b,b, function=recurrent,synapse=tau_synapse)
